4ec1ad930f49f2b42c60dafdb5b300a569abe6fd
darivericoneric <darivericoneric@outlook.com>
2025-05-27 15:34:40 -0600

    se crea olvide-seccion

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,16 +1,24 @@
 const formularioLogin =(req, res) => {
     res.render('auth/login', {
-        
+        pagina: 'Iniciar Sesión',
     });
 }
 
 const formularioRegistro =(req, res) => {
     res.render('auth/registro', {
-        pagina: 'Crear Cuenta'
+        pagina: 'Crear Cuenta',
     });
 }
 
+const formularioOlvidePassword =(req, res) => {
+    res.render('auth/olvide-password', {
+        pagina: 'Recupera tu acceso a Bienes Raíces',
+    });
+}
+
+// Exportamos varias funciones desde un mismo archivo
 export {
     formularioLogin,
-    formularioRegistro
+    formularioRegistro,
+    formularioOlvidePassword
 }
